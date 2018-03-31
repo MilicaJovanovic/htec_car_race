148617fe810bb00cb3a8cb33e5edf1507a2904db
Milica Jovanovic <j.milica95@yahoo.com>
2018-03-31 23:26:50 +0200

    Cars from JSON file displayed in the table

--- app/main.js
+++ app/main.js
@@ -1,6 +1,33 @@
 let allCars = [];
 
 /*
+ * Generates rows depending on cars number 
+ */
+const generateRow = (carsNumber) => {
+  const rowsNumber = Math.ceil(carsNumber/3);
+  for(let i = 0; i < rowsNumber; i++ ) {
+    const row = {
+      id: "row" + (i+1)
+    }
+    $.get('../templates/row.html', (template, textStatus, jqXhr) => {
+      $('#cars').append(Mustache.render($(template).filter('#row').html(), row))
+    });
+  }
+}
+
+/*
+ * Generates cars defined in JSON 
+ */
+const generateCar = (carsNumber, cars) => {
+  for(let i = 0; i < carsNumber; i++) {
+    const currentRowNumber = Math.ceil((i+1)/3);
+    const currentRowId = "#row" + currentRowNumber;
+    $.get('../templates/car.html', (template, textStatus, jqXhr) => {
+      $(currentRowId).append(Mustache.render($(template).filter('#car').html(), cars[i]))
+    });
+  }
+}
+/*
  * Loads data from API
  * Generates content 
  */
@@ -11,6 +38,9 @@ const loadJSON = _ => {
     .then(data => {
       allCars = data.cars;
       console.log(allCars);
+      const carsNumber = data.cars.length;
+      generateRow(carsNumber);
+      generateCar(carsNumber, data.cars);   
     })
   })
   .catch(error => {throw new Error(error)})
