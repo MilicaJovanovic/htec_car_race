af7a88f6c62221cafa5c4286c26f6fd918938d2f
Milica Jovanovic <j.milica95@yahoo.com>
2018-03-31 22:04:23 +0200

    Reading data from static JSON file

--- app/main.js
+++ app/main.js
@@ -1,1 +1,19 @@
-console.log('HTEC CAR RACE');
+let allCars = [];
+
+/*
+ * Loads data from API
+ * Generates content 
+ */
+const loadJSON = _ => {
+  fetch('../json/data.json')
+  .then(response => {
+    response.json()
+    .then(data => {
+      allCars = data.cars;
+      console.log(allCars);
+    })
+  })
+  .catch(error => {throw new Error(error)})
+}
+
+loadJSON()
