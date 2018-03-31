da02ea816fd98d8888d02b6f619c7736d0799399
Milica Jovanovic <j.milica95@yahoo.com>
2018-03-31 21:41:31 +0200

    Initial commit - project created

---
+++ app/main.js
@@ -0,0 +1,1 @@
+console.log('HTEC CAR RACE');
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  entry: './app/main.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
+  }
+};
